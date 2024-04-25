05933ca3535742bbd84359de5614b46792ab4e02
jryxxx <1499948365@qq.com>
2024-04-25 15:37:54 +0800

    edit

--- van.py
+++ van.py
@@ -2,8 +2,7 @@ import torch
 import torch.nn as nn
 
 from timm.models.layers import DropPath
-from mmcv.cnn.utils.weight_init import (constant_init, normal_init,
-                                        trunc_normal_init)
+from mmcv.cnn.utils.weight_init import constant_init, normal_init, trunc_normal_init
 from torch.nn.modules.utils import _pair as to_2tuple
 from mmseg.models.builder import BACKBONES
 
@@ -14,7 +13,15 @@ import warnings
 
 
 class Mlp(nn.Module):
-    def __init__(self, in_features, hidden_features=None, out_features=None, act_layer=nn.GELU, drop=0., linear=False):
+    def __init__(
+        self,
+        in_features,
+        hidden_features=None,
+        out_features=None,
+        act_layer=nn.GELU,
+        drop=0.0,
+        linear=False,
+    ):
         super().__init__()
         out_features = out_features or in_features
         hidden_features = hidden_features or in_features
@@ -44,7 +51,8 @@ class AttentionModule(nn.Module):
         super().__init__()
         self.conv0 = nn.Conv2d(dim, dim, 5, padding=2, groups=dim)
         self.conv_spatial = nn.Conv2d(
-            dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3)
+            dim, dim, 7, stride=1, padding=9, groups=dim, dilation=3
+        )
         self.conv1 = nn.Conv2d(dim, dim, 1)
 
     def forward(self, x):
@@ -76,56 +84,72 @@ class SpatialAttention(nn.Module):
 
 class Block(nn.Module):
 
-    def __init__(self,
-                 dim,
-                 mlp_ratio=4.,
-                 drop=0.,
-                 drop_path=0.,
-                 act_layer=nn.GELU,
-                 linear=False,
-                 norm_cfg=dict(type='SyncBN', requires_grad=True)):
+    def __init__(
+        self,
+        dim,
+        mlp_ratio=4.0,
+        drop=0.0,
+        drop_path=0.0,
+        act_layer=nn.GELU,
+        linear=False,
+        norm_cfg=dict(type="SyncBN", requires_grad=True),
+    ):
         super().__init__()
         self.norm1 = build_norm_layer(norm_cfg, dim)[1]
         self.attn = SpatialAttention(dim)
-        self.drop_path = DropPath(
-            drop_path) if drop_path > 0. else nn.Identity()
+        self.drop_path = DropPath(drop_path) if drop_path > 0.0 else nn.Identity()
 
         self.norm2 = build_norm_layer(norm_cfg, dim)[1]
         mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = Mlp(in_features=dim, hidden_features=mlp_hidden_dim,
-                       act_layer=act_layer, drop=drop, linear=linear)
+        self.mlp = Mlp(
+            in_features=dim,
+            hidden_features=mlp_hidden_dim,
+            act_layer=act_layer,
+            drop=drop,
+            linear=linear,
+        )
         layer_scale_init_value = 1e-2
         self.layer_scale_1 = nn.Parameter(
-            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True
+        )
         self.layer_scale_2 = nn.Parameter(
-            layer_scale_init_value * torch.ones((dim)), requires_grad=True)
+            layer_scale_init_value * torch.ones((dim)), requires_grad=True
+        )
 
     def forward(self, x, H, W):
         B, N, C = x.shape
         x = x.permute(0, 2, 1).view(B, C, H, W)
-        x = x + self.drop_path(self.layer_scale_1.unsqueeze(-1).unsqueeze(-1)
-                               * self.attn(self.norm1(x)))
-        x = x + self.drop_path(self.layer_scale_2.unsqueeze(-1).unsqueeze(-1)
-                               * self.mlp(self.norm2(x)))
+        x = x + self.drop_path(
+            self.layer_scale_1.unsqueeze(-1).unsqueeze(-1) * self.attn(self.norm1(x))
+        )
+        x = x + self.drop_path(
+            self.layer_scale_2.unsqueeze(-1).unsqueeze(-1) * self.mlp(self.norm2(x))
+        )
         x = x.view(B, C, N).permute(0, 2, 1)
         return x
 
 
 class OverlapPatchEmbed(nn.Module):
-    """ Image to Patch Embedding
-    """
+    """Image to Patch Embedding"""
 
-    def __init__(self,
-                 patch_size=7,
-                 stride=4,
-                 in_chans=3,
-                 embed_dim=768,
-                 norm_cfg=dict(type='SyncBN', requires_grad=True)):
+    def __init__(
+        self,
+        patch_size=7,
+        stride=4,
+        in_chans=3,
+        embed_dim=768,
+        norm_cfg=dict(type="SyncBN", requires_grad=True),
+    ):
         super().__init__()
         patch_size = to_2tuple(patch_size)
 
-        self.proj = nn.Conv2d(in_chans, embed_dim, kernel_size=patch_size, stride=stride,
-                              padding=(patch_size[0] // 2, patch_size[1] // 2))
+        self.proj = nn.Conv2d(
+            in_chans,
+            embed_dim,
+            kernel_size=patch_size,
+            stride=stride,
+            padding=(patch_size[0] // 2, patch_size[1] // 2),
+        )
         self.norm = build_norm_layer(norm_cfg, embed_dim)[1]
 
     def forward(self, x):
@@ -140,50 +164,64 @@ class OverlapPatchEmbed(nn.Module):
 
 @BACKBONES.register_module()
 class VAN(BaseModule):
-    def __init__(self,
-                 in_chans=3,
-                 embed_dims=[64, 128, 256, 512],
-                 mlp_ratios=[8, 8, 4, 4],
-                 drop_rate=0.,
-                 drop_path_rate=0.,
-                 depths=[3, 4, 6, 3],
-                 num_stages=4,
-                 linear=False,
-                 pretrained=None,
-                 init_cfg=None,
-                 norm_cfg=dict(type='SyncBN', requires_grad=True)):
+    def __init__(
+        self,
+        in_chans=3,
+        embed_dims=[64, 128, 256, 512],
+        mlp_ratios=[8, 8, 4, 4],
+        drop_rate=0.0,
+        drop_path_rate=0.0,
+        depths=[3, 4, 6, 3],
+        num_stages=4,
+        linear=False,
+        pretrained=None,
+        init_cfg=None,
+        norm_cfg=dict(type="SyncBN", requires_grad=True),
+    ):
         super(VAN, self).__init__(init_cfg=init_cfg)
 
-        assert not (init_cfg and pretrained), \
-            'init_cfg and pretrained cannot be set at the same time'
+        assert not (
+            init_cfg and pretrained
+        ), "init_cfg and pretrained cannot be set at the same time"
         if isinstance(pretrained, str):
-            warnings.warn('DeprecationWarning: pretrained is deprecated, '
-                          'please use "init_cfg" instead')
-            self.init_cfg = dict(type='Pretrained', checkpoint=pretrained)
+            warnings.warn(
+                "DeprecationWarning: pretrained is deprecated, "
+                'please use "init_cfg" instead'
+            )
+            self.init_cfg = dict(type="Pretrained", checkpoint=pretrained)
         elif pretrained is not None:
-            raise TypeError('pretrained must be a str or None')
+            raise TypeError("pretrained must be a str or None")
 
         self.depths = depths
         self.num_stages = num_stages
         self.linear = linear
 
-        dpr = [x.item() for x in torch.linspace(0, drop_path_rate,
-                                                sum(depths))]  # stochastic depth decay rule
+        dpr = [
+            x.item() for x in torch.linspace(0, drop_path_rate, sum(depths))
+        ]  # stochastic depth decay rule
         cur = 0
 
         for i in range(num_stages):
-            patch_embed = OverlapPatchEmbed(patch_size=7 if i == 0 else 3,
-                                            stride=4 if i == 0 else 2,
-                                            in_chans=in_chans if i == 0 else embed_dims[i - 1],
-                                            embed_dim=embed_dims[i])
+            patch_embed = OverlapPatchEmbed(
+                patch_size=7 if i == 0 else 3,
+                stride=4 if i == 0 else 2,
+                in_chans=in_chans if i == 0 else embed_dims[i - 1],
+                embed_dim=embed_dims[i],
+            )
 
-            block = nn.ModuleList([Block(dim=embed_dims[i],
-                                         mlp_ratio=mlp_ratios[i],
-                                         drop=drop_rate,
-                                         drop_path=dpr[cur + j],
-                                         linear=linear,
-                                         norm_cfg=norm_cfg)
-                                   for j in range(depths[i])])
+            block = nn.ModuleList(
+                [
+                    Block(
+                        dim=embed_dims[i],
+                        mlp_ratio=mlp_ratios[i],
+                        drop=drop_rate,
+                        drop_path=dpr[cur + j],
+                        linear=linear,
+                        norm_cfg=norm_cfg,
+                    )
+                    for j in range(depths[i])
+                ]
+            )
             norm = nn.LayerNorm(embed_dims[i])
             cur += depths[i]
 
@@ -192,19 +230,17 @@ class VAN(BaseModule):
             setattr(self, f"norm{i + 1}", norm)
 
     def init_weights(self):
-        print('init cfg', self.init_cfg)
+        print("init cfg", self.init_cfg)
         if self.init_cfg is None:
             for m in self.modules():
                 if isinstance(m, nn.Linear):
-                    trunc_normal_init(m, std=.02, bias=0.)
+                    trunc_normal_init(m, std=0.02, bias=0.0)
                 elif isinstance(m, nn.LayerNorm):
-                    constant_init(m, val=1.0, bias=0.)
+                    constant_init(m, val=1.0, bias=0.0)
                 elif isinstance(m, nn.Conv2d):
-                    fan_out = m.kernel_size[0] * m.kernel_size[
-                        1] * m.out_channels
+                    fan_out = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
                     fan_out //= m.groups
-                    normal_init(
-                        m, mean=0, std=math.sqrt(2.0 / fan_out), bias=0)
+                    normal_init(m, mean=0, std=math.sqrt(2.0 / fan_out), bias=0)
         else:
             super(VAN, self).init_weights()
 
@@ -234,3 +270,9 @@ class DWConv(nn.Module):
     def forward(self, x):
         x = self.dwconv(x)
         return x
+
+
+if __name__ == "__main__":
+    model = VAN()
+    input = torch.randn(1, 3, 224, 224)
+    model(input)
